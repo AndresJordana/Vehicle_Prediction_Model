be785fc853567fe0d43dee30cfeac508ac5fb0df
AndresJordana <105009140+AndresJordana@users.noreply.github.com>
2024-12-11 11:03:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ MODEL_URL = 'https://raw.githubusercontent.com/AndresJordana/Vehicle_Prediction_
 MODEL_LOCAL_PATH = 'model_realistic.pkl'
 
 # Set OpenAI API key
-open.api_key = st.secrets["OPENAI_API_KEY"]
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 
 # Download the model file if not already present locally
